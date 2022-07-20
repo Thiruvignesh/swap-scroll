c05878b18998131672f72a961d3db442dca15e7b
Thiru <mastiffthiru>
2022-07-20 12:51:16 +0530

    added gsap scrolltrigger setup simple scrolltrigger for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {SwapScrollService} from "./swap-scroll.service";
+import {gsap} from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'swap-scroll';
   scrollDisabled = false;
+
+  @ViewChild('main') main: ElementRef;
+  @ViewChild('container') container: ElementRef;
+  @ViewChildren('.box') boxList: QueryList<ElementRef>;
+
+
   constructor(private scrollService: SwapScrollService) {
+  }
+
+  ngAfterViewInit() {
+    gsap.registerPlugin(ScrollTrigger);
+    const scrollBox = gsap.timeline({
+      scrollTrigger: {
+        scroller:this.main.nativeElement,
+        trigger: this.container.nativeElement,
+        pin: true,
+        start: "top top",
+        end: "bottom bottom",
+        markers: true,
+        toggleActions: "play none none reverse"
+      }
+    });
+    scrollBox.to(this.container.nativeElement, { y: 30, opacity: 0 });
   }
 
   enableScroll() {
